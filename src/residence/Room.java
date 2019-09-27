2e6b0c3ceec494c385740ffdd7a4d0155514c023
Morten Olsen <morten222@hotmail.com>
2019-09-27 21:00:02 +0200

    Room added TC01: Test for setSize() & getSize() pass

---
+++ src/residence/Room.java
@@ -0,0 +1,13 @@
+package residence;
+
+public class Room {
+    private int size;
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
