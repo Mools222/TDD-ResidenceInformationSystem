f6d9138c19decddcdb8419e2e4dfbd8837efc9d5
Morten Olsen <morten222@hotmail.com>
2019-09-27 21:27:40 +0200

    TC08: Test for getNumberOfApartments() pass

--- src/residence/BuildingBlock.java
+++ src/residence/BuildingBlock.java
@@ -1,14 +1,19 @@
 package residence;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BuildingBlock {
     private String address;
+    private ArrayList<Apartment> apartmentList;
 
-    public BuildingBlock(String address) {
+    public BuildingBlock(String address, Apartment...apartments) {
         this.address = address;
+        apartmentList = new ArrayList<>(Arrays.asList(apartments));
     }
 
     public int getNumberOfApartments() {
-        return 0;
+        return apartmentList.size();
     }
 
     public int getSize() {
--- test/residence/BuildingBlockTest.java
+++ test/residence/BuildingBlockTest.java
@@ -2,7 +2,7 @@ package residence;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BuildingBlockTest {
     @Test
@@ -15,7 +15,7 @@ class BuildingBlockTest {
 
     @Test
     public void testCreateBuildingBlock2() {
-        BuildingBlock buildingBlock = new BuildingBlock("Lille Gade 14, 7430 Ikast");
-        assertEquals(0, buildingBlock.getNumberOfApartments());
+        BuildingBlock buildingBlock = new BuildingBlock("Lille Gade 14, 7430 Ikast", new Apartment(new Room(40, Type.ROOM)), new Apartment(new Room(40, Type.ROOM)));
+        assertEquals(2, buildingBlock.getNumberOfApartments());
     }
 }
