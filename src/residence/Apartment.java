d9243ec47565682b501ac47208173cbcfde0d036
Morten Olsen <morten222@hotmail.com>
2019-09-27 21:15:16 +0200

    TC05: Test for getSize() pass

--- src/residence/Apartment.java
+++ src/residence/Apartment.java
@@ -1,11 +1,25 @@
 package residence;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Apartment {
+    private ArrayList<Room> roomList;
+
+    public Apartment(Room... rooms) {
+        roomList = new ArrayList<>(Arrays.asList(rooms));
+    }
+
+    public int getSize() {
+        int size = 0;
+        for (Room room: roomList) {
+            size += room.getSize();
+        }
+        return size;
+    }
+
     public int getRoomCount() {
         return 0;
     }
 
-    public int getSize() {
-        return 0;
-    }
 }
--- test/residence/ApartmentTest.java
+++ test/residence/ApartmentTest.java
@@ -14,7 +14,7 @@ class ApartmentTest {
 
     @Test
     public void testCreateApartment2() {
-        Apartment apartment = new Apartment();
+        Apartment apartment = new Apartment(new Room(12, Type.KITCHEN), new Room(7, Type.BATHROOM), new Room(15, Type.ROOM), new Room(25, Type.ROOM));
         assertEquals(59, apartment.getSize());
     }
 }
