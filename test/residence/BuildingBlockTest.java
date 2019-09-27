1e77f2d85d3734ced61414140c80e5bf706b0856
Morten Olsen <morten222@hotmail.com>
2019-09-27 21:23:35 +0200

    TC07: Test for getAddress() add

--- test/residence/BuildingBlockTest.java
+++ test/residence/BuildingBlockTest.java
@@ -7,9 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class BuildingBlockTest {
     @Test
     public void testCreateBuildingBlock1() {
-        BuildingBlock buildingBlock = new BuildingBlock();
+        BuildingBlock buildingBlock = new BuildingBlock("Store Gade 19, 7400 Herning");
         assertEquals(0, buildingBlock.getNumberOfApartments());
         assertEquals(0, buildingBlock.getSize());
+        assertEquals(0, buildingBlock.getAddress());
     }
 
 }
